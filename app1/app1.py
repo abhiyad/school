088401a9669cf399ccd931eea3d8531be739414c
abhiyad <abhishekcms611@gmail.com>
2022-06-05 00:33:35 +0530

    added deployment files

---
+++ app1/app1.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+counter = 0
+
+@app.route('/')
+def home():
+	global counter
+	counter+=1
+	return "hello world app1 " + str(counter)
+
+
+if __name__ == "__main__":
+	counter = 0
+	port = int(os.environ.get('PORT', 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
